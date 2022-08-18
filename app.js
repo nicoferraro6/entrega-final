d544b4451046e99d0b8f54196376a7fb6f85cb5b
nicoferraro6 <nicolasferraro6@gmail.com>
2022-08-18 00:51:29 -0300

    entrega-correcta

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
 // ---Primer entrega proyecto final---
 
-function CampeonesMundiales(pais, continente, vecesCampeon){
-    this.pais = pais;
-    this.continente = continente;
-    this.vecesCampeon = vecesCampeon;
+    class Campeon{
+        constructor (pais, continente, anio){
+        this.pais = pais;
+        this.continente = continente;
+        this.vecesCampeon = anio;
+
+        this.votos = [];
+        }
+
+        agregarVoto(votos){
+            this.votos.push(votos);
+        }
+
+        eliminarVoto(votos){
+            this.votos.pop();
+        }
     }
     
-    let uruguay = new CampeonesMundiales("Uruguay", "América", 2);
-    let brasil = new CampeonesMundiales("Brasil", "América", 5);
-    let italia = new CampeonesMundiales("Italia", "Europa", 4);
-    let alemania = new CampeonesMundiales("Alemania", "Europa", 4);
-    let francia = new CampeonesMundiales("Francia", "Europa", 2);
-    let inglaterra = new CampeonesMundiales("Inglaterra", "Europa", 1);
-    let espania = new CampeonesMundiales("España", "Europa", 1);
     
-    console.log(uruguay, brasil, italia, alemania, francia, inglaterra, espania)
+    const campeonArgentina = new Campeon("Argentina", "America", 1986);
+
+    campeonArgentina.agregarVoto(1);
+    campeonArgentina.agregarVoto(1);
+    campeonArgentina.agregarVoto(1);
+    campeonArgentina.agregarVoto(1);
+    campeonArgentina.agregarVoto(1);
+    campeonArgentina.agregarVoto(1);
+
+    alert("Argentina campeon del 86 recibió " + campeonArgentina.votos.length + " votos");
+
     
-    
-    const campeonesMundiales = ["Brasil", "Uruguay", "Italia", "Alemania", "Francia", "Inglaterra", "España"];
-    
-    campeonesMundiales.push("Argentina")
-    
-    console.log(campeonesMundiales)
-    
-    
-    let paisesCampeones = prompt("Ingresa un país campeon mundialista")
-    
-    const paisCampeon = [];
-    
-    while (paisesCampeones != "ESC"){
-        paisCampeon.push(paisesCampeones)
-        paisesCampeones = prompt("Ingresa otro país campeón o ESC para finalizar")
-    }
-    
-    for (const element of paisCampeon) {
-    alert("Países campeones: " + element)
-    }
