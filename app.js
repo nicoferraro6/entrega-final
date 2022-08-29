fdaf34be02e3b5222482b6d5d82e6cfb5ff4c04b
nicoferraro6 <nicolasferraro6@gmail.com>
2022-08-29 20:06:16 -0300

    carrito

--- app.js
+++ app.js
@@ -1,17 +1,124 @@
-// Ejercicio de Entrega
+//crear el constructor que nos sirve como referencia para nuestro objeto prod
+
+class Producto {
+    constructor(nombre, precio, detalle){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.detalle = detalle;
+    }
+}
+
+const prod1 = new Producto("Campera Adidas", 50000, "Running")
+const prod2 = new Producto("Zapatillas Nike", 70000, "AirForce")
+const prod3 = new Producto("Remera Puma", 45000, "Training")
+const prod4 = new Producto("Chomba Lacoste", 10000, "LifeStyle")
+const prod5 = new Producto("Pantalon Under Armour", 30000, "Training")
+
+const listaProd = [];
+
+listaProd.push(prod1,prod2,prod3,prod4,prod5)
+
+console.log(listaProd)
+
+//defino el DOM
+
+let form = document.getElementById('form')
+let inputProd = document.getElementById('iprod')
+
+let mostrarProdSolos = document.getElementById('mostrarProdSolos')
+let mostrarTodosProd = document.getElementById('mostrarTodosProd')
+let btnMostrarProd = document.getElementById('btnMostrar')
+let bandera = false
+
+//capturo los hijos de form
+let iprod = form.children[1].value;
+let iprecio = form.children[3].value;
+let idetalle = form.children[5].value;
 
 
-let formulario = document.getElementById("form")
 
-formulario.addEventListener("submit", datosForm)
+//definir los eventos
 
-function datosForm(e){
-//cancelamos por las dudas
+form.addEventListener('submit', agregarProd)
+btnMostrarProd.addEventListener('click', mostrarTodosLosProd)
+
+//funciones
+
+//funcion para comprobar el ingreso de datos en los input
+
+//pongo en focus el input
+
+inputProd.focus()
+
+function validarDatos() {
+    iprod = form.children[1].value;
+    iprecio = form.children[3].value;
+    idetalle = form.children[5].value;
+    console.log(iprod)
+    console.log(iprecio)
+    console.log(idetalle)
+
+    if(iprod == '' || iprecio == '' || idetalle == ''){
+        alert("Error debe completar todos los campos para continuar")
+        inputProd.focus()
+        bandera = false;
+    } else {
+        bandera = true;
+    }
+}
+
+
+
+//funcion agregar prod al array
+
+function agregarProd(e){
+    //cancelamos el comportamiento del evento
+    e.preventDefault()
+    validarDatos();
+    if(bandera == true){
+        let opcion = confirm("Esta seguro de agregar el prod")
+        if(opcion == true){
+            //captura el dato del input en formato objeto del DOM
+            let datos = e.target
+            //enviamos los datos al array
+            listaProd.push(new Producto(iprod, iprecio, idetalle))
+            datos.children[1].value = "";
+            datos.children[3].value = "";
+            datos.children[5].value = "";
+            //traer el innerHTML 
+            mostrarProdSolos.innerHTML = ""
+            //agregar la funcion que lso muestra en el dom 
+            mostrarUnProd()
+            inputProd.focus()
+        } else {
+            alert("No se agregara el usuario")
+        }
+
+        } else {
+            inputProd.focus
+        }
+
+}
+
+//funcion para mostrar el DOM del ultimo prod ingresado
+
+const mostrarUnProd = () =>{
+    mostrarProdSolos.innerHTML = `
+    <h3>Ultimo producto ingresado:</h3>
+    <p><strong>Producto: </strong>${iprod}</p>
+    <p><strong>Precio: </strong>${iprecio}</p>
+    <p><strong>Detalle: </strong>${idetalle}</p>`
+}
+
+//funcion para mostrar todos los producto ingresados
+
+function mostrarTodosLosProd (e){
     e.preventDefault();
-// capturar los datos de los input
-    let datos = e.target
-// obtenemos los datos ingresados e enviados en el evento
-    console.log(datos.nombre.value)
-    console.log(datos.edad.value)
-    console.log(datos.password.value)
+    mostrarTodosProd.innerHTML = '<h3>Listado de todos los productos</h3>';
+    for(const datos of listaProd){
+        mostrarTodosProd.innerHTML += `
+        <p><strong>Producto: </strong>${datos.nombre}</p>
+        <p><strong>Precio: </strong>${datos.precio}</p>
+        <p><strong>Detalle: </strong>${datos.detalle}</p> `
+    }
 }
